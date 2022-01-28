1de50225581c82ca4a3bae552202304996b48ed0
Karol Sontak <karolsontak@gmail.com>
2022-01-28 15:49:01 -0300

    Teste restart

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import validator from './validator.js';
+//import validator from "./validator.js";
+const validator = {
+    valid() {
+      // e.preventDefault();
+  
+      let cardNumber = document.getElementsByName("cardNumber");
+  
+      let cardNumberValue = cardNumber.value;
+      console.log(cardNumberValue);
+      console.log(typeof cardNumberValue);
+  
+    }
+  } 
 
-console.log(validator);
+console.log('hello carol');
+
+const validate = document.getElementById("button");
+
+validate.addEventListener("click", validator.valid);
