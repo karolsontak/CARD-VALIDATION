cfaddbd994ff01eecc877b9824acba3b259a7418
Karol Sontak <karolsontak@gmail.com>
2022-02-01 10:35:23 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
-//import validator from "./validator.js";
-const validator = {
-    valid() {
-      // e.preventDefault();
-  
-      let cardNumber = document.getElementsByName("cardNumber");
-  
-      let cardNumberValue = cardNumber.value;
-      console.log(cardNumberValue);
-      console.log(typeof cardNumberValue);
-  
-    }
-  } 
+let cardNumber = document.getElementById("cardNumberId")  //Pega o valor do input 
+let validCard = cardNumber.value.replace(/(\d{4})?(\d{4})?(\d{4})?(\d{4})/, "$1-$2-$3-$4")
 
-console.log('hello carol');
+console.log(validCard) = "ok"
 
-const validate = document.getElementById("button");
 
-validate.addEventListener("click", validator.valid);
+const validateButton = document.getElementById("buttonId"); //Pega o valor do botão 
+validateButton.addEventListener("click", function (e){
+    e.preventDefault();   
+}) 
+console.log(validateButton.isValid(buttonId))
+
+
+
+
+
+
+//function valid (e){
+//  e.preventDefault()   //(e) = event - preventDefault = cancela o evento
+  
+
+//
+    
+
+
+
+//const validCard = cardNumber.value.replace(/(\d{4})?(\d{4})?(\d{4})?(\d{4})/, "$1-$2-$3-$4");
+
+//const validateButton = document.getElementById("buttonId").addEventListener("click", function (e){
+// e.preventDefault();
+//console.log(validateButton.isValid(buttonId))
+//console.log(validate)  
+//});
+
+
+//validate.addEventListener("click", function (e){
+//  e.preventDefault();
+//const result = validator.isValid(validCard);
+//console.log(result); 
+
+
+
+
+
+// MASCARA
+//function maskify(cardNumber){
+//  return cardNumber.split("").map((letter,i) => i < cardNumber.lenght - 4 ? "#" : letter).join("");
+// ou
+// maskify(isValid) {
+// return isValid.replace(/.(?=.{4})/g, "#");
--- src/validator.js
+++ src/validator.js
@@ -1,12 +1,44 @@
-const validator = {
-  valid() {
-    // e.preventDefault();
+//import {} from './index.js'
 
-    let cardNumber = document.getElementsByName("cardNumber");
+const validator = { 
+   maskify: function(nmber){
+     let mask = ""
+     for (let ind=0; ind < nmber.length; ind++){
+      if (ind >= ((nmber.length)-4)){
+        mask = mask + nmber.charAt(ind)
+      }else{
+        mask= mask + "#"
+      }
+     }
+     return mask
+   }
 
-    let cardNumberValue = cardNumber.value;
-    console.log(cardNumberValue);
-    console.log(typeof cardNumberValue);
+  ,isValid: function(numeroCartao){
+    const cardList = Array.from(numeroCartao)
+    let cardListRev = cardList.reverse()
+    let sum = 0
+    for (let i=0; i < cardListRev.length; i++){
+      if (i % 2 != 0){
+        cardListRev[i] = (Number(cardListRev[i])) * 2
+        if(cardListRev[i] > 9){
+          cardListRev[i] = cardListRev[i] - 9
+          sum = sum + Number(cardListRev[i])
+        }else{
+          sum = sum + (cardListRev[i])
+        }
+      }else{
+        sum = sum + Number(cardListRev[i])
+      }
+    }
+    if (sum % 10 === 0){
+      return numeroCartao = true
 
+    }else{
+      return numeroCartao = false
+    }
+
+    // return console.log(cardListRev, sum)
   }
-} 
+}
+
+export default validator;
